37b3d7dd2332e63a5bdab554ae75b78e123dcaa1
unknown <zdravkodelic1993@gmail.com>
2026-03-16 23:52:12 +0100

    chore: update project config for TypeScript migration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
@@ -10,9 +9,7 @@ import { ThemeProvider } from "./context/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
 );
