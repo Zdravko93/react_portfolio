a94c626373941486682a2a40b640715421d616e1
unknown <zdravkodelic1993@gmail.com>
2026-03-16 22:47:39 +0100

    chore: add TypeScript configuration files

---
+++ src/types/assets.d.ts
@@ -0,0 +1,34 @@
+declare module "*.mp3" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*module.css" {
+  const src: { [key: string]: string };
+  export default src;
+}
+
+declare module "*.css" {
+  const src: string;
+  export default src;
+}
